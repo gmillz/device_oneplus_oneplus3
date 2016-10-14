c031bc49cd8d7e9005ff1ea8ef71563f9f01c9d4
Martinusbe <martinusbe@gmail.com>
2016-10-14 17:53:54 +0200

    Revert "oneplus3: Add device specific healthd"
    This reverts commit 6148665e921c8954abd52aaf0eb833c56f87f8c2.
    
    Change-Id: Ic184dd8c88276e5f1fd94d4cd409b109659e2a65

--- device.mk
+++ device.mk
@@ -149,6 +149,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
