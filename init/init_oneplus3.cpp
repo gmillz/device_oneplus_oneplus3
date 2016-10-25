LineageOS/android_device_oneplus_oneplus3

op3: Fix init extension build on N
Change-Id: I26ee05334af1f3edc151a741c756382dc57e13ab

init/init_oneplus3.cpp
@@ -28,10 +28,11 @@
  */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 
+#include <cutils/properties.h>
 #include "vendor_init.h"
-#include "property_service.h"
 #include "log.h"
 #include "util.h"
 
@@ -92,11 +93,11 @@ void vendor_load_properties() {
     char rf_version[PROP_VALUE_MAX];
     int rc;
 
-    rc = property_get("ro.cm.device", device);
+    rc = property_get("ro.cm.device", device, NULL);
     if (!rc || strncmp(device, "oneplus3", PROP_VALUE_MAX))
         return;
 
-    property_get("ro.boot.rf_version", rf_version);
+    property_get("ro.boot.rf_version", rf_version, NULL);
 
     if (strstr(rf_version, "11") || strstr(rf_version, "31")) {
         /* Chinese/America*/

gmillz/device_oneplus_oneplus3

init/init_oneplus3.cpp
@@ -28,10 +28,11 @@
  */
 
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 
+#include <cutils/properties.h>
 #include "vendor_init.h"
-#include "property_service.h"
 #include "log.h"
 #include "util.h"
 
@@ -92,11 +93,11 @@ void vendor_load_properties() {
     char rf_version[PROP_VALUE_MAX];
     int rc;
 
-    rc = property_get("ro.product.device", device);
+    rc = property_get("ro.product.device", device, NULL);
     if (!rc || strncmp(device, "OnePlus3", PROP_VALUE_MAX))
         return;
 
-    property_get("ro.boot.rf_version", rf_version);
+    property_get("ro.boot.rf_version", rf_version, NULL);
 
     if (strstr(rf_version, "11") || strstr(rf_version, "31")) {
         /* Chinese/America*/