LineageOS/android_device_oneplus_oneplus3

op3: Add seccomp policy for mediaserver
Change-Id: I39236a8ccacbb6a232a292d88a18c9eac75ec0d0

BoardConfig.mk
@@ -190,6 +190,9 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# SecComp
+BOARD_SECCOMP_POLICY := $(PLATFORM_PATH)/sepolicy
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 

gmillz/device_oneplus_oneplus3

BoardConfig.mk
@@ -192,6 +192,9 @@ BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# SecComp
+BOARD_SECCOMP_POLICY := $(PLATFORM_PATH)/seccomp
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 